e4bb82178cef304cf14c533b4eb236dc8b0b6eaf
umairarshadbutt <umair6622@gmail.com>
2020-08-07 20:48:43 +0500

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ActiveRecord::Base
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   def tag_list
       self.tags.collect do |tag|
         tag.name
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_222438) do
+ActiveRecord::Schema.define(version: 2020_08_07_150252) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
